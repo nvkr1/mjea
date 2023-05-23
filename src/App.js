f60e4cf7a92d616399665d4205c4be1ec68545e5
Ikimonogatari <tuvshuutuvshinjargal21@gmail.com>
2023-05-23 13:03:56 +0800

    removed files

--- src/App.js
+++ src/App.js
@@ -1,22 +1,8 @@
-import React, {useState, useEffect} from 'react';
-import { Routes, Route} from 'react-router-dom';  
-import Preloader from './Components/Preloader';
-import ScrollTopBtn from './Components/ScrollTopBtn';
-import AboutPage from './pages/AboutPage';
-import ServicePage from './pages/ServicePage';
-import HomeOne from './pages/HomeOne';
-import HomeTwo from './pages/HomeTwo';
-import News from './pages/News';
-import NewsDetails from './pages/NewsDetails';
-import TeamPage from './pages/TeamPage';
-import FaqPage from './pages/FaqPage';
-import PricingPage from './pages/PricingPage';
-import ServiceDetails from './pages/ServiceDetails';
-import HomeThree from './pages/HomeThree';
-import HomeFour from './pages/HomeFour';
-import ProjectPage from './pages/ProjectPage';
-import ContactPage from './pages/ContactPage';
-
+import React, { useState, useEffect } from "react";
+import { Routes, Route } from "react-router-dom";
+import Preloader from "./Components/Preloader";
+import ScrollTopBtn from "./Components/ScrollTopBtn";
+import HomeFour from "./pages/HomeFour";
 
 function App() {
   const [loading, setLoading] = useState(true);
@@ -28,29 +14,16 @@ function App() {
   }, []);
 
   if (loading) {
-    return <Preloader/>
+    return <Preloader />;
   } else {
     return (
-    <>
-      <ScrollTopBtn/>
-      <Routes>
-        <Route path="/" element={<HomeOne />} />
-        <Route path="/homeTwo" element={<HomeTwo />} />
-        <Route path="/homeThree" element={<HomeThree />} />
-        <Route path="/homeFour" element={<HomeFour />} />
-        <Route path="/about" element={<AboutPage />} />
-        <Route path="/services" element={<ServicePage />} />
-        <Route path="/team" element={<TeamPage />} />
-        <Route path="/faq" element={<FaqPage />} />
-        <Route path="/contact" element={<ContactPage />} />
-        <Route path="/pricing" element={<PricingPage />} />
-        <Route path="/newsDetails" element={<NewsDetails />} />
-        <Route path="/servicesDetails" element={<ServiceDetails />} />
-        <Route path="/news" element={<News />} />
-        <Route path="/projects" element={<ProjectPage />} />
-      </Routes>
-    </>
-    )
+      <>
+        <ScrollTopBtn />
+        <Routes>
+          <Route path='/' element={<HomeFour />} />
+        </Routes>
+      </>
+    );
   }
 }
 
